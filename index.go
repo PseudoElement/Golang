8c26bd32cba7de23388e2c4d9585e0b993263006
PseudoElement <bubamainer@mail.ru>
2024-03-05 05:50:38 +0300

    init

---
+++ index.go
@@ -0,0 +1,50 @@
+package main
+import "fmt"
+
+// type T int
+// type K int
+
+func find[T any](arr []T, cond func(T) bool) interface{} {
+    for i := 0; i < len(arr); i++ {
+        el := arr[i]
+        if cond(el) {
+            return el
+        }
+    }
+
+    return nil
+}
+
+func filter(arr []string, fn func(value string, ind int) bool) []string{
+    var filtered []string
+    for i, el := range arr {
+        needPush := fn(el, i);
+        if needPush {
+            filtered = append(filtered, el);
+        }
+    }
+    return filtered;
+}
+
+func main() {
+    var arr = []int{1, 2, 3, 4, 5}
+	let asdasd = asdas;
+    var found = find(arr, func (num int) bool{
+        return num >= 5;
+    })
+    
+    fmt.Println(found)
+
+    const str string = "Siska";
+    strToLower := strings.ToLower(str);
+    
+    var strSlice []string
+    for _, char := range strToLower {
+        strSlice = append(strSlice, string(char))
+    }
+    var sLetterArr []string
+    sLetterArr = filter(strSlice, func(v string, ind int) bool {
+        return string(v) == "s";
+    })
+    fmt.Println(sLetterArr)
+}
