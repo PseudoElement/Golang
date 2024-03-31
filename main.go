a991274a816f9ac9bcca072177a5e94be9261232
PseudoElement <bubamainer@mail.ru>
2024-04-01 02:47:41 +0300

    delete req

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	ApiService "go-server/src/api"
 	Middlewares "go-server/src/api/middlewares"
 	Oneinch "go-server/src/modules/1inch"
 	Crud "go-server/src/modules/crud"
@@ -12,12 +11,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type User struct {
-    UserID string `json:"userId"`
-    ID     int `json:"id"`
-    Title  string `json:"title"`
-    Body   string `json:"body"`
-}
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true);
@@ -27,13 +20,6 @@ func main() {
 
 	Oneinch.SetOneinchRoutes(r);
 	Crud.SetCrudRoutes(r);
-
-	user, err := ApiService.Post[User]("https://jsonplaceholder.typicode.com/posts", User{"229", 230, "Hello Borroq", "Sintolidze"}, map[string]string{})
-	if err != nil{
-		panic(err);
-	}
-
-	fmt.Println("POST_RESPONSE - ", user);
 
 	fmt.Println("Listening port 8080...");
 	log.Fatal(http.ListenAndServe(":8080", r))
