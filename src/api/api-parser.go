dce99d7ce7e7649ec84a971a3b81193d954beed8
PseudoElement <bubamainer@mail.ru>
2024-05-22 15:53:55 +0300

    delete commas

--- src/api/api-parser.go
+++ src/api/api-parser.go
@@ -13,59 +13,59 @@ import (
 
 func MapQueryParams(req *http.Request, queryParamsKeys ...string) (map[string]string, errors_module.ErrorWithStatus) {
 	mapppedParams := make(map[string]string)
-	query := req.URL.Query();
+	query := req.URL.Query()
 
 	for _, key := range queryParamsKeys {
-		param := query.Get(key);
+		param := query.Get(key)
 		if param == "" {
 			return mapppedParams, errors_module.IncorrectQueryParams()
 		}
-		mapppedParams[key] = param 
+		mapppedParams[key] = param
 	}
 
-	return mapppedParams, nil;
+	return mapppedParams, nil
 }
 
 /* Parses body to provided generic type and restricts body size to 1MB */
-func ParseReqBody[T any](w http.ResponseWriter, req *http.Request)(T, errors_module.ErrorWithStatus){
+func ParseReqBody[T any](w http.ResponseWriter, req *http.Request) (T, errors_module.ErrorWithStatus) {
 	req.Body = http.MaxBytesReader(w, req.Body, 1048576)
-	decoder := json.NewDecoder(req.Body);
-	decoder.DisallowUnknownFields();
+	decoder := json.NewDecoder(req.Body)
+	decoder.DisallowUnknownFields()
 
-	body := new(T);
-	err := decoder.Decode(&body);
+	body := new(T)
+	err := decoder.Decode(&body)
 
 	if hasEmpty, emptyField := validators_main.HasEmptyField(*body); hasEmpty {
-		return *body, errors_module.EmptyFieldInJson(emptyField);
+		return *body, errors_module.EmptyFieldInJson(emptyField)
 	}
-	
+
 	if err != nil {
-        var syntaxError *json.SyntaxError
-        var unmarshalTypeError *json.UnmarshalTypeError
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
 
-        switch {
-			case errors.As(err, &syntaxError):
-				return *body, errors_module.BadlyFormedJson();
+		switch {
+		case errors.As(err, &syntaxError):
+			return *body, errors_module.BadlyFormedJson()
 
-			case errors.Is(err, io.ErrUnexpectedEOF):
-				return *body, errors_module.BadlyFormedJson();
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return *body, errors_module.BadlyFormedJson()
 
-			case errors.As(err, &unmarshalTypeError):
-				return *body, errors_module.InvalidValueJson(unmarshalTypeError)
+		case errors.As(err, &unmarshalTypeError):
+			return *body, errors_module.InvalidValueJson(unmarshalTypeError)
 
-			case strings.HasPrefix(err.Error(), "json: unknown field "):
-				return *body, errors_module.UnknownFieldJson(err.Error())
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			return *body, errors_module.UnknownFieldJson(err.Error())
 
-			case errors.Is(err, io.EOF):
-				return *body, errors_module.EmptyBody()
+		case errors.Is(err, io.EOF):
+			return *body, errors_module.EmptyBody()
 
-			case err.Error() == "http: request body too large":
-				return *body, errors_module.TooLargeBody()
+		case err.Error() == "http: request body too large":
+			return *body, errors_module.TooLargeBody()
 
-			default:
-				return *body, errors_module.IncorrectBody()
-        }
-    }
+		default:
+			return *body, errors_module.IncorrectBody()
+		}
+	}
 
-	return *body, nil;
+	return *body, nil
 }
